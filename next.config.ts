476d98fc1f6e6084afcebb78588310c2d444d78f
Gabriela Ramos <gabriela.ramos@agetic.gob.bo>
2026-08-21 16:26:12 -0400

    modificacion de iconos y supabase

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [new URL("https://readymadeui.com/**")],
+  },
 };
 
 export default nextConfig;
